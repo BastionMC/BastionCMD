3f80e9e13ae6b4fa973aa30e312052522e0e166a
JaegerwaldDev <86529399+JaegerwaldDev@users.noreply.github.com>
2024-01-02 16:24:27 +0100

    made the server page cooler, added start.bat

---
+++ client/start.bat
@@ -0,0 +1,15 @@
+:: The purpose of this file is to test the program/have it being
+:: linked as a shortcut to easily start the program. This file
+:: is the one being generated as a shortcut by the installer.
+
+:: In-dev comment: should we automatically install Python with
+:: the installer? Leave your answers here as comments.
+
+:: Start the main.py file.
+@echo off
+python main.py
+
+:: Ending loop so the window does not close after the program
+:: is done running. Maybe not the most performant option.
+:end
+goto end
