408af1d3947302a3db21eeabf7980fba027331df
JaegerwaldDev <86529399+JaegerwaldDev@users.noreply.github.com>
2024-01-07 19:06:29 +0100

    did help

--- client/feature/help.py
+++ client/feature/help.py
@@ -1,2 +1,11 @@
+import cticf, os, sys
+from pathlib import Path
+file_path = Path(sys.argv[0]).parent.absolute()
+
+ui = cticf.rfile(os.path.join(file_path, "feature/help.cticf"))
+ui = {
+    "list": ui[0]
+}
+
 def run():
-    print("insert help here")
+    print("\n" + ui["list"] + "\n")
